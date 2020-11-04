6979be0bb176751dac92cda4b19b1ae27b1878f3
David Simon Tetruashvili <david.tetruashvili@kcl.ac.uk>
2020-11-04 18:53:25 +0100

    Add gruvbox colors and set boarder size

--- config.def.h
+++ config.def.h
@@ -1,17 +1,40 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const unsigned int borderpx = 3;        /* border pixel of windows */
+static const unsigned int snap = 32;       /* snap pixel */
+static const unsigned int gappih = 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov = 10;       /* vert outer gap between windows and screen edge */
+static const int smartgaps = 0;        /* 1 means no outer gap when there is only one window */
+static const int showbar = 1;        /* 0 means no bar */
+static const int topbar = 1;        /* 0 means bottom bar */
+static const char* fonts[] = { "monospace:size=10" };
+static const char dmenufont[] = "monospace:size=10";
+static const char col_gray1[] = "#222222";
+static const char col_gray2[] = "#444444";
+static const char col_gray3[] = "#bbbbbb";
+static const char col_gray4[] = "#eeeeee";
+static const char col_cyan[] = "#005577";
+
+static const char col_gruvbox_bg0[] = "#282828";
+static const char col_gruvbox_bg1[] = "#3c3836";
+static const char col_gruvbox_bg2[] = "#504945";
+static const char col_gruvbox_bg3[] = "#665c54";
+static const char col_gruvbox_bg4[] = "#7c6f64";
+static const char col_gruvbox_fg[] = "#ebdbb2";
+static const char col_gruvbox_orange[] = "#d65d0e";
+static const char col_gruvbox_red[] = "#cc241d";
+static const char col_gruvbox_green[] = "#98971a";
+static const char col_gruvbox_yellow[] = "#d79921";
+static const char col_gruvbox_blue[] = "#458588";
+static const char col_gruvbox_purple[] = "#b16286";
+static const char col_gruvbox_aqua[] = "#689d6a";
+static const char col_gruvbox_gray[] = "#a89984";
+
+static const char col_wallpaper_bg[] = "#20241d";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
